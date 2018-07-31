81165b5bf40773f984acaa5b3f1301828ca95ef3
enamaro <enamaro@wanadoo.es>
2018-07-31 19:57:04 +0200

    Web-service tareas Json y Xml.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,12 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
 class DefaultController extends Controller
 {
@@ -37,5 +43,47 @@ class DefaultController extends Controller
             array('tarea'=>$tarea, 'url_atras'=>$url_atras) );
     }
 
-    
+    /**
+     * @Route("/tareas.json", name="tareas_json")
+     */
+    public function tareasJsonAction()
+    {
+        $repository = $this->getDoctrine()->getRepository('AppBundle:Tarea');
+
+        $tareas = $repository->findAllOrderedByDescripcion();
+
+        $encoders = array(new XmlEncoder(), new JsonEncoder());
+        $normalizers = array(new ObjectNormalizer());
+
+        $serializer = new Serializer($normalizers, $encoders);
+
+        $jsonContenido = $serializer->serialize($tareas, 'json');
+        
+        $response = new Response();
+        $response->headers->set('Content-type', 'application/json');
+        $response->setContent($jsonContenido);
+        return $response;
+    }
+
+    /**
+     * @Route("/tareas.xml", name="tareas_xml")
+     */
+    public function tareasXmlAction()
+    {
+        $repository = $this->getDoctrine()->getRepository('AppBundle:Tarea');
+
+        $tareas = $repository->findAllOrderedByDescripcion();
+
+        $encoders = array(new XmlEncoder(), new JsonEncoder());
+        $normalizers = array(new ObjectNormalizer());
+
+        $serializer = new Serializer($normalizers, $encoders);
+
+        $jsonContenido = $serializer->serialize($tareas, 'xml');
+        
+        $response = new Response();
+        $response->headers->set('Content-type', 'application/xml');
+        $response->setContent($jsonContenido);
+        return $response;
+    }
 }
